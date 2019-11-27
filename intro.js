e5f2f42d10a76cfbe22e9cf0488eee7fa55f3033
Sebas5LV <56948689+Sebas5LV@users.noreply.github.com>
2019-11-27 19:30:08 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,10 @@
+//Dato númerico
+var edad = 25;
+
+//string, nos sirve para poner texto e incluso concatenar
+var nombre = "Sebas"
+
+var gravedad = 9.8 m/s^2
+
+//array y objeto es parecido, pero los objetos podemos darle atributos.
+ var dias =["Lunes","Martes","Miércoles","Jueves", "Viernes","Sábado","Domingo"];
